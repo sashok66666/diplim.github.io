c9e101263692108b26b2ec7bb1b3d19207a31383
Aleksander Karavaev <karavaev.1993@mail.ru>
2020-03-02 09:41:25 +0300

    Главная страница

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
       }),
     new HtmlWebpackPlugin({
       inject: false,
-      template: './src/index.html',
+      template: './src/main.html',
       filename: 'index.html'
     }),
     new WebpackMd5Hash(),
