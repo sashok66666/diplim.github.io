3a44e2687ca0fbc00463741de3485715f885d88f
Aleksander Karavaev <karavaev.1993@mail.ru>
2020-03-06 20:51:07 +0300

    Блок 'история коммитов'

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+import Glide from '@glidejs/glide';
 import "../pages/style.css";
 import "../images/facebook.svg";
 import "../images/github.svg";
@@ -6,3 +7,12 @@ import "../images/html.svg";
 import "../images/css.svg";
 import  "../images/javascript.svg";
 import "../images/webpack.svg";
+import "../images/image-01.png";
+import "../images/image-03.png";
+import "../images/image-04.png";
+
+
+new Glide('.glide',{
+    type: 'carousel',
+    perView: 3
+}).mount()
