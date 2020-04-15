dd7441ccb35a7627e74cf88ed8cac46ae341209e
Aleksander Karavaev <karavaev.1993@mail.ru>
2020-04-15 14:27:41 +0300

    https

--- src/js/constants/NEW_SAPI.js
+++ src/js/constants/NEW_SAPI.js
@@ -1,5 +1,5 @@
 const newSapi = {
-    baseUrl: 'http://newsapi.org/v2/everything?',
+    baseUrl: 'https://newsapi.org/v2/everything?',
     apiKey: 'apiKey=d9a08bfcb5264cb3b7c1fd52bae77503',
     pageSize: 'pageSize=100',
     language: 'language=ru'
